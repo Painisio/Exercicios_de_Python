b5012caa32859c74e0ad5905bca136d0ba4944ba
ANISIO PORTELLA <92547400+Painisio@users.noreply.github.com>
2023-09-14 11:24:12 -0300

    Create Exercicio_010.py

---
+++ Exercicio_010.py
@@ -0,0 +1,13 @@
+# Faça um programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+
+g_c = float(input("Qual a temperatura em Celsius?")) # entrada de variavel
+txt = "A temperatura convertida em Fahrenheint é: " # Textinho
+g_f = (g_c*9/5)+32 # calculo da conversão
+print(txt, g_f) # saída
+
+"""
+Obtive a seguinte saída do codigo:
+
+Qual a temperatura em Celsius?93.33
+A temperatura convertida em Fahrenheint é:  199.994
+"""
