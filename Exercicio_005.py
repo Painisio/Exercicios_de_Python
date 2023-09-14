8256a90d418b04e540fe141b04b7ceaaa5e0c9e2
ANISIO PORTELLA <92547400+Painisio@users.noreply.github.com>
2023-09-14 10:37:48 -0300

    Create Exercicio_005.py

---
+++ Exercicio_005.py
@@ -0,0 +1,13 @@
+# Faça um Programa que converta metros para centímetros.
+
+metro = float(input("Insira a metragem pra ser Convertida: ")) # Variavél de entrada 
+cent = metro*100 # Conversão em centimetros
+texto = "centimetros." #Texto
+print(cent, texto) # Saída
+
+"""
+A saída do código  é a seguinte:
+
+Insira a metragem pra ser Convertida: 1.5
+150.0 centimetros
+"""
