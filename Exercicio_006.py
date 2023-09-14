32b62fdc85b4a90f54e4f071e570a51676edda26
ANISIO PORTELLA <92547400+Painisio@users.noreply.github.com>
2023-09-14 10:43:10 -0300

    Create Exercicio_006.py

---
+++ Exercicio_006.py
@@ -0,0 +1,14 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+ray = float(input("Defina o Raio do Círculo: ")) # Variável de Entrada
+pi = 3.1415 # Valor assumido de π (pi)
+area_of_circle = (ray**2)*pi # Calculo
+texto = "A área do círculo para o raio informado é: " # Texto pra fazer o charme, e não ficar apenas a matemática =)
+print(texto, area_of_circle) # Sáida do Código
+
+"""
+A sáida exibida do código é a seguinte:
+
+Defina o Raio do Círculo: 2
+A área do círculo para o raio informado é:  12.566
+"""
