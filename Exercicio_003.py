eb5978d723bd2729a310b0303d4c649eef6f2710
ANISIO PORTELLA <92547400+Painisio@users.noreply.github.com>
2023-09-14 10:17:36 -0300

    Create Exercicio_003.py

---
+++ Exercicio_003.py
@@ -0,0 +1,13 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+n1 = int(input("Insira o primeiro número: ")) # Primeira variável
+n2 = int(input("Insira o segundo número: ")) # Segunda variável
+soma = n1 + n2 # Soma o primeiro com o segundo número
+texto = "A soma dos dois números é: " # Texto a ser exibido!
+
+print(texto, soma)
+
+# Segue a saida obtida no Código:
+# Insira o primeiro número: 1
+# Insira o segundo número: 2
+# A soma dos dois números é:  3
