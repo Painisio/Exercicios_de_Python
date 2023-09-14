cba4176499a9199e39c82b2770edb52b8a8180c7
ANISIO PORTELLA <92547400+Painisio@users.noreply.github.com>
2023-09-14 10:08:23 -0300

    Create Exercicio_002.py

---
+++ Exercicio_002.py
@@ -0,0 +1,10 @@
+# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+numero = input('Digite um numero: ')
+texto = 'O numero informado foi'
+print(texto, numero)
+
+# Esse é a saida do código: 
+
+# Digite um numero: 159
+# O numero informado foi 159
